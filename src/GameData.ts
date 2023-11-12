a4cfe76af0abf2b40af4e64076d5e93dbe3ddced
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2023-11-11 18:38:15 -0800

    add more pasta

--- src/GameData.ts
+++ src/GameData.ts
@@ -1,7 +1,7 @@
 import { useLocalStorage } from "@mantine/hooks";
 import rawData from "./assets/data.txt?raw";
 
-const GAME_OFFSET = 19;
+const GAME_OFFSET = 15;
 
 interface GameSaveData {
   id: number;
